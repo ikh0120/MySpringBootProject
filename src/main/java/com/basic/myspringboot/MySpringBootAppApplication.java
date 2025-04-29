76b2505bbd51a0bfdcba8dd3b5d9627066d0f058
ikh0120 <lkh50635164@gmail.com>
2025-04-29 17:47:41 +0900

    Add src/main/resources/banner.txt

--- src/main/java/com/basic/myspringboot/MySpringBootAppApplication.java
+++ src/main/java/com/basic/myspringboot/MySpringBootAppApplication.java
@@ -1,13 +1,34 @@
-package com.basic.myspringboot;
+package com.basic.myspringboot; //베이스 패키지임
+/* 베이스 패키지 밑이 아니면 @Component 같은거 못씀
+ * 하위 패키지여야지만 사용 가능함
+ */
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+/*
+@SpringBootApplication =
+: @SpringBootConfiguration + @EnableAutoConfiguration + @ComponentScan
+   ( @Configuration )
+*/
 @SpringBootApplication
 public class MySpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootAppApplication.class, args);
+		//SpringApplication.run(MySpringBootAppApplication.class, args);
+		//12page 복사
+		SpringApplication application = new SpringApplication(MySpringBootAppApplication.class);
+		//기본적으로 WebApplicationType은 웹어플리케이션(SERVLET)이다.????
+		application.setWebApplicationType(WebApplicationType.SERVLET);
+		application.run(args);
+
+	}
+
+	@Bean
+	public String hello(){
+		return "hello springboot";
 	}
 
 }
