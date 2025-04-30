55c038c234c8ecf16060901bb5ba39d0dabb8445
ikh0120 <lkh50635164@gmail.com>
2025-04-30 14:33:56 +0900

    Builder Pattern Learning

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,24 +1,42 @@
 package com.basic.myspringboot.runner;
 
+import com.basic.myspringboot.property.MyBootProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements ApplicationRunner { // 추상메서드 불러오기 단축키: <Ctrl> + i
 
+    // 환경변수를 @Value에 넣는데변수 이름에 공백 혹은 대소문자 지켜 넣기
     @Value("${myboot.name}")
     private String name;
 
     @Value("${myboot.age}")
     private int age;
 
+    @Autowired
+    private Environment environment;
+
+    @Autowired //@Component가 있기에 쓸 수 있음
+    private MyBootProperties properties;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("${myboot.name} = "+name);
-        System.out.println("${myboot.age} = "+age);
+        System.out.println("${myboot.name} = " + name);
+        System.out.println("${myboot.age} = " + age);
+        //Environment.getProperty("변수") : 변수는 환경변수를 가져올 때 대소문자 구분을 안하지만 변수 이름은 구분함
+        System.out.println("${myboot.fullName} = " + environment.getProperty("myboot.fullName"));
 
+        System.out.println();
+        System.out.println("MyBootProperties getName() = " + properties.getName());
+        System.out.println("MyBootProperties getAge() = " + properties.getAge());
+        System.out.println("MyBootProperties getFullName() = " + properties.getFullName());
+        System.out.println();
+        System.out.println("설정된 Port 번호 = "+environment.getProperty("local.server.port"));
         // foo 라는 VM 아규먼트가 있는지 확인하기
         System.out.println("VM 아규먼트 foo : " + args.containsOption("foo"));
         // bar 라는 Program 아규먼트가 있는지 확인하기
