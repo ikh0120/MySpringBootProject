cb15a827f583d451df01e716b9e8710451d0c1a9
ikh0120 <lkh50635164@gmail.com>
2025-04-30 14:43:33 +0900

    How to use @Profile

--- src/main/java/com/basic/myspringboot/config/ProdConfig.java
+++ src/main/java/com/basic/myspringboot/config/ProdConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("test")
-/* 이거로 ProdConfig, TestConfig의 같은 이름의 빈을
- application.properties에서 구분해줌*/
+// 이거로 ProdConfig, TestConfig의 같은 이름의 빈을
+// application.properties에서 구분해줌
+@Profile("prod")
 @Configuration
-public class TestConfig {
+public class ProdConfig {
     @Bean
     public CustomVO customerVO(){
         return CustomVO.builder()  //CustomVOBuilder
-                .mode("테스트환경")  //두개는 변수 넣기
+                .mode("운영환경")  //두개는 변수 넣기
                 .rate(0.5)         //두개는 변수 넣기
                 .build();   //bulider()로 CustomBuilder 타입으로 변경 후 값을 다 넣고 다시 CustomVO 타입으로 변경
 
