5643df538d3b9a8eacef097f5e0a223e7f8bf42f
ikh0120 <lkh50635164@gmail.com>
2025-04-30 14:37:00 +0900

    Builder Pattern Learning

--- src/main/java/com/basic/myspringboot/config/TestConfig.java
+++ src/main/java/com/basic/myspringboot/config/TestConfig.java
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TestConfig {
     @Bean
-    public CustomerVO customerVO(){
+    public CustomVO customerVO(){
+        return CustomVO.builder()  //CustomVOBuilder
+                .mode("테스트환경")  //두개는 변수 넣기
+                .rate(0.5)         //두개는 변수 넣기
+                .build();   //bulider()로 CustomBuilder 타입으로 변경 후 값을 다 넣고 다시 CustomVO 타입으로 변경
 
     }
 }
