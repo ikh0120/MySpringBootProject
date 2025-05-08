03d6b23aafc02863cffbb366aaf84fab285f2d01
ikh0120 <lkh50635164@gmail.com>
2025-05-09 06:45:37 +0900

    entity/Customer.java, application.properties, application-prod.properties, application-test.properties add

--- src/main/java/com/basic/myspringboot/MySpringBootAppApplication.java
+++ src/main/java/com/basic/myspringboot/MySpringBootAppApplication.java
@@ -18,7 +18,6 @@ public class MySpringBootAppApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(MySpringBootAppApplication.class, args);
-		//12page 복사
 		SpringApplication application = new SpringApplication(MySpringBootAppApplication.class);
 		//기본적으로 WebApplicationType은 웹어플리케이션(SERVLET)이다.????
 		application.setWebApplicationType(WebApplicationType.SERVLET);
---
+++ src/main/java/com/basic/myspringboot/entity/Customer.java
@@ -0,0 +1,27 @@
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+//DB에 Customer라는 테이블을 만들어 줌
+//MyBatis의 VO(Value Object) 클래스와 유사
+@Entity
+@Table(name = "customers")//테이블명: customers // 이 어노테이션이 없으면 테이블명을 클래스 이름으로 만들어줌
+@Getter @Setter
+public class Customer {
+    //Primary Key, pk 값을 persistence provider(구현체인 Hibernate) 가 알아서 결정해라
+//    @Id @GeneratedValue(strategy = GenerationType.AUTO) //Auto로 할 경우 테이블명_seq 테이블이 생성됨
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // Maria DB라 IDENTITY로 할것이다
+    private Long id;
+
+    //중복을 허용하지 않고, null 값을 허용하지 않음
+    @Column(unique = true, nullable = false)
+    private String customerId;
+
+    //null 값을 허용하지 않음
+    @Column(nullable = false)
+    private String customerName;
+
+
+}
