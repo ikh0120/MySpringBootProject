e1fbe1b762ff9326187898ac156bbe431286e33f
ikh0120 <lkh50635164@gmail.com>
2025-05-12 08:01:01 +0900

    user Controller getUsers Add

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -6,10 +6,33 @@ import com.basic.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * HTTP POST 요청을 처리하는 메서드
+ *
+ * - 클라이언트(Postman 등)로부터 JSON 형식의 사용자 데이터를 수신함
+ * - @RequestBody 어노테이션을 통해 JSON을 User 객체로 변환함 (역직렬화)
+ * - 변환된 User 객체를 JPA의 userRepository.save()로 DB에 저장
+ * - 저장된 User 객체를 응답으로 반환 (ID 및 생성 시간 등 포함)
+ *
+ * 예시:
+ * POST http://localhost:8080/api/users
+ * {
+ *   "name": "스프링",
+ *   "email": "spring@a.com"
+ * }
+ *
+ * 응답:
+ * {
+ *   "id": 1,
+ *   "name": "스프링",
+ *   "email": "spring@a.com",
+ *   "createdAt": "2025-05-12T07:29:34.523809"
+ * }
+ */
 
 //@RestController = @Controller + @ResponseBody
 @RestController
@@ -50,10 +73,33 @@ public class UserRestController {
      * 이는 테스트 시 Mock 객체를 명확하게 주입하기 더 적합하고 테스트 코드가 더 유연해짐
      */
 
-    //Request Mapping
+    //Request Mapping 중 하나인 PostMapping
     @PostMapping
     public User create(@RequestBody User user) {
         return userRepository.save(user);
+    }
+    /**Postman 사용법
+    Post Man에서 POST로 드롭박스를 바꾸고 텍스트필드에 http://localhost:8080/api/users 입력 후
+    Headers 메뉴에서 Key를 Content-Type으로 Value를 application/json으로 설정
+    Body 메뉴에서 라디오버튼 중 raw 선택 후 JSON으로 변경(Headers 메뉴에서 Content-Type을 application/json으로 설정해서 JSON이 기본 값)후
+    {
+     "name":"스프링",
+     "email":"spring@a.com"
+    }
+    라는 JSON 데이터를 넣어주고 http://localhost:8080/api/users를 넣은 텍스트필드의 오른쪽에 있는 Send 버튼을 클릭하면
+    밑의 Body 메뉴에
+    {
+        "id": 1,
+        "name": "스프링",
+        "email": "spring@a.com",
+        "createdAt": "2025-05-12T07:29:34.523809"
+    }
+    가 들어가면서 자동으로 내 users 테이블에 값 저장됨
+    */
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 
 }
