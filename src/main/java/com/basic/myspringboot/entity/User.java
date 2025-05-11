5dbf9602edb9de135775d88ee0df0d13ae98ca19
ikh0120 <lkh50635164@gmail.com>
2025-05-12 05:57:08 +0900

    User Entity class Add

---
+++ src/main/java/com/basic/myspringboot/entity/User.java
@@ -0,0 +1,34 @@
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+/** 테이블은 @SpringBootApplication이 붙은 메인 메서드를 실행시켜야지만 자동으로 생성된다*/
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    // 자동으로 User 객체가 생성될 때 현재 시간을 넣도록 설정
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    // 자동으로 객체가 생성될 때
+    // ID는 자동으로 증가하는 기본키이고 createdAt은 현재 시간을 넣도록 설정한 것이어서
+    // 우리가 넣어야 하는 값은 name과 email 두개가 다임
+}
