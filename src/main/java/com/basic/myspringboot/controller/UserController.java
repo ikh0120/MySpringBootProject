faeeaf539b879972387dfadf34f7d9dbaffa51f9
ikh0120 <lkh50635164@gmail.com>
2025-05-14 22:57:13 +0900

    index link add

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller //@RestController(=@ResponseBody + @Controller)가 아닌 그냥 @Controller
 public class UserController {
     @GetMapping("/thymeleaf")
-    public String leaf(Model model) {
-        model.addAttribute("name", "basic");
+    public String leaf(Model model) { //Model 객체: Spring Framework에서 UI 상에서 쓸 데이터를 저장해주는 객체
+        model.addAttribute("name", "스프링 부트!");
         return "leaf";
     }
 }
