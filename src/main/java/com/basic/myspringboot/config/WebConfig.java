df8b139d7c1967579ef6bd6a42e7abf3aef366bb
ikh0120 <lkh50635164@gmail.com>
2025-05-14 22:19:14 +0900

    index.html add

--- src/main/java/com/basic/myspringboot/config/WebConfig.java
+++ src/main/java/com/basic/myspringboot/config/WebConfig.java
@@ -6,11 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
+    @Override /** resources/mobile/index.html 인식시켜주는 메서드 */
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/mobile/**")
                 //반드시 mobile 다음에 / 을 주어야 한다.
-                .addResourceLocations("classpath:/mobile/")
+                .addResourceLocations("classpath:/mobile/") // classpath는 target/classes을 의미
                 .setCachePeriod(20);//20초
     }
 }
